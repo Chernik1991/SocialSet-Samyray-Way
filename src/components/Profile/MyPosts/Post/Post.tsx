ddc1da91a491100b8da341ae9f1ff725b0df974f
Chernik1991 <5059585@mail.ru>
2022-04-29 10:55:08 +0300

    Samyray-way 17

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './Post.module.css';
+
+ export type PropsType={
+    message:string
+    likesCount:string
+}
+
+const Post = (props:PropsType) => {
+
+    return (
+        <div className={s.item}>
+            <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' />
+            { props.message }
+            <div>
+                <span>like</span> { props.likesCount }
+            </div>
+        </div>
+    )
+}
+
+export default Post;
