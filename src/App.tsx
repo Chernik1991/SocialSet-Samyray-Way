03da00efe6390e7b580b77dc291484c4c56c01bb
Chernik1991 <5059585@mail.ru>
2022-05-24 16:28:18 +0300

    Samyray-way 49 Users

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,21 @@ import {News} from './components/New/News';
 import {Music} from './components/Music/Music';
 import {Setting} from './components/Setting/Setting';
 import {DialogsContainer} from './components/Dialogs/DialogsContainer';
+import {Users} from "./components/Users/Users";
 
 const App = () => {
-    const ProfilePostsData = () => {
-        return <Profile/>}
-    const DialogsDialogsData = () => {
-        return <DialogsContainer/> }
-    return (
+      return (
         <BrowserRouter>
             <div className="app-wrapper">
                 <Header/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    <Route exact path="/profile" render={ProfilePostsData}/>
-                    <Route exact path="/dialogs" render={DialogsDialogsData}/>
+                    <Route exact path="/profile" render={() =><Profile/>}/>
+                    <Route exact path="/dialogs" render={()=><DialogsContainer/>}/>
                     <Route exact path="/news" render={() => <News/>}/>
                     <Route exact path="/music" component={() => <Music/>}/>
                     <Route exact path="/setting" component={() => <Setting/>}/>
+                    <Route exact path="/users" render={() => <Users/>}/>
                 </div>
             </div>
         </BrowserRouter>);
